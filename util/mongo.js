7aee6efb38aaadc989595a7b9745b55d13d38b10
practice-stack1 <vadim3d2011@gmail.com>
2022-05-11 14:57:36 +0300

    -

--- util/mongo.js
+++ util/mongo.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL =
+    "mongodb+srv://Vadimas:nextpizza@cluster0.7dfc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 if (!MONGO_URL) {
-  throw new Error(
-    'Please define the MONGO_URL environment variable inside .env.local'
-  )
+    throw new Error(
+        "Please define the MONGO_URL environment variable inside .env.local"
+    )
 }
 
 /**
@@ -16,25 +17,25 @@ if (!MONGO_URL) {
 let cached = global.mongoose
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+    cached = global.mongoose = { conn: null, promise: null }
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
+    if (cached.conn) {
+        return cached.conn
     }
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
 }
 
 export default dbConnect
